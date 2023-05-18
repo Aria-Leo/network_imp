3034b3aa7a587da246922db0e044364861b54748
ywtai <632910913@qq.com>
2023-05-18 10:42:19 +0800

    add n_classes to data_load

--- mnist_loader.py
+++ mnist_loader.py
@@ -4,11 +4,11 @@ import pickle
 import numpy as np
 
 
-def load_data(file_path, mode='rb'):
+def load_data(file_path, mode='rb', n_classes=10):
     f = gzip.open(file_path, mode)
     tr_d, va_d, te_d = pickle.load(f, encoding='bytes')
     f.close()
     training_inputs = tr_d[0]
-    training_results = np.eye(10)[tr_d[1]]
+    training_results = np.eye(n_classes)[tr_d[1]]
     tr_d = (training_inputs, training_results)
     return tr_d, va_d, te_d
