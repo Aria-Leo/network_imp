9c54d72191149ebedea9c93815c1c4c5dbfe77da
ywtai <632910913@qq.com>
2023-06-30 19:24:41 +0800

    add data samples

--- data_loader.py
+++ data_loader.py
@@ -34,7 +34,7 @@ class PKLDataLoader:
 class CSVDataLoader:
 
     @staticmethod
-    def load(zip_file_path, visualization=False, aspect_ratio=1, channels=1):
+    def load(zip_file_path, visualization=False, aspect_ratio=1, channels=1, samples=100):
         # train validation test
         processed_data = [None, None, None]
         with zipfile.ZipFile(zip_file_path, mode='r') as zip_filer:
@@ -43,6 +43,11 @@ class CSVDataLoader:
                     continue
                 with zip_filer.open(file_name, mode='r') as filer:
                     data = pd.read_csv(filer)
+                    random_idx = np.random.permutation(data.shape[0])
+                    if isinstance(samples, int) and samples < data.shape[0]:
+                        data = data.iloc[random_idx[:samples]]
+                    elif isinstance(samples, float) and samples < 1:
+                        data = data.iloc[random_idx[:int(data.shape[0] * samples)]]
                     features, labels = data.drop('label', axis=1).values, data['label'].values
                     if len(features.shape) == 2 and visualization:
                         area = features.shape[1]
